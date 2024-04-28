2f149e3cf076c8c9927b02d25f87def3a1b28ecc
priscillah barasa <beverlybarasa@gmail.com>
2024-04-28 22:00:55 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    handleTodo(e.target.new_description.value)
+  })
+})
+
+function handleTodo(description){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent= `${description}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('list').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
